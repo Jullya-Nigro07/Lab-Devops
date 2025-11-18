8d0190d3e677a6a827f5cacdd6589a5bb31ca0fe
Jullya-Nigro <nigrojullya07@gmail.com>
2025-11-18 12:27:49 -0300

    subindo testes

--- test_app.py
+++ test_app.py
@@ -7,23 +7,21 @@ if not hasattr(werkzeug, '__version__'):
 
 class APITestCase(unittest.TestCase): 
     @classmethod 
-    def setUpClass(cls): 
-        # Criação do cliente de teste 
+    def setUpClass(cls):  
         cls.client = app.test_client()
     
-    def test_home(self):
+    def test_status(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json, {"message": "API is running"})
+
+    def test_itens(self):
+        response = self.client.get('/items')
+        self.assertEqual(response.status_code, 200)
 
     def test_login(self):
         response = self.client.post('/login')
         self.assertEqual(response.status_code, 200)
-        self.assertIn('access_token', response.json)
-
-    def test_protected_no_token(self):
-        response = self.client.get('/protected')
-        self.assertEqual(response.status_code, 401)
+        self.assertTrue(response.is_json)
 
 if __name__ == '__main__':
     unittest.main()
